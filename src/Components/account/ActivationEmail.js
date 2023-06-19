14a0b9cb9fad9751426ef2da59a48317ca2d801c
DeepCoomer <deepcoomer198@gmail.com>
2023-06-19 23:14:57 +0530

    Initial Commit

--- src/Components/account/ActivationEmail.js
+++ src/Components/account/ActivationEmail.js
@@ -1,28 +1,38 @@
-import { Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import React from 'react'
-import { makeStyles } from '@material-ui/styles';
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import { useViewportSize } from "@mantine/hooks";
 
 const ActivationEmail = () => {
+  const useStyles = makeStyles({
+    para: {
+      display: "inline-block",
+      position: "relative",
+      top: "200px",
+      left: "300px",
+    },
+    paramobile: {
+      display: "inline-block",
+      position: "relative",
+      top: "200px",
+      left: "3px",
+    },
+  });
 
-    const useStyles = makeStyles({
-        para:{
-            display: 'inline-block',
-            position: 'relative',
-            top: '200px',
-            left: '300px'
-        }
-    })
+  const classes = useStyles();
 
-    const classes = useStyles();
+  const { width } = useViewportSize();;
 
-    return (
-        <>
-          <Box>
-              <Typography variant="h4" className={classes.para}>Please Check Your Email an activation link has been send!!!.</Typography>
-          </Box>  
-        </>
-    )
-}
+  return (
+    <>
+      <Box>
+        <Typography variant="h4" className={width>768? classes.para: classes.paramobile}>
+          Please Check Your Email an activation link has been send!!!.
+        </Typography>
+      </Box>
+    </>
+  );
+};
 
-export default ActivationEmail
+export default ActivationEmail;
